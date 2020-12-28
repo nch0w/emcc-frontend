26142a04f8123af0f2834c0556828255f364ba24
Neil Chowdhury <neil.chowdhury@outlook.com>
2020-12-28 14:33:25 -0800

    update to use SES

--- server/mail/signup.js
+++ server/mail/signup.js
@@ -2,14 +2,19 @@ const transporter = require("./transporter");
 
 async function signupMail(emailAddress, token, userId) {
   await transporter.sendMail({
-    from: '"Exeter Math Club" <exetermathclub@gmail.com>',
+    from: '"Exeter Math Club" <no-reply@exetermathclub.com>',
     to: emailAddress,
     subject: "✅ [EMCC] Please verify your email address",
     text: `Please verify your email address at ${process.env.SITE_URL}/verify/${token}${userId}.`,
     html: `
     Thanks for creating an account for the Exeter Math Club Competition! 
     <br />
-    Please verify your email address at <a href="${process.env.SITE_URL}/verify/${token}${userId}">${process.env.SITE_URL}/verify/${token}${userId}</a>.`
+    <br />
+    Please verify your email address at <a href="${process.env.SITE_URL}/verify/${token}${userId}">${process.env.SITE_URL}/verify/${token}${userId}</a>.
+    <br />
+    <br />
+    If you need help, please message <a href="exetermathclub@gmail.com">exetermathclub@gmail.com</a>.
+    `
   });
 }
 
--- server/mail/transporter.js
+++ server/mail/transporter.js
@@ -1,12 +1,18 @@
-require("dotenv").config();
-
 const nodemailer = require("nodemailer");
+const aws = require("aws-sdk");
+
+// const transporter = nodemailer.createTransport({
+//   service: "Gmail",
+//   auth: {
+//     user: "exetermathclub@gmail.com",
+//     pass: process.env.GMAIL_PASSWORD
+//   }
+// });
+
 const transporter = nodemailer.createTransport({
-  service: "Gmail",
-  auth: {
-    user: "exetermathclub@gmail.com",
-    pass: process.env.GMAIL_PASSWORD
-  }
+  SES: new aws.SES({
+    apiVersion: "2010-12-01"
+  })
 });
 
 module.exports = transporter;

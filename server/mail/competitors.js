8b9d791602c41a6035da72aac1e9a3e46d12260e
Alan Bu <50249961+Icosehedron@users.noreply.github.com>
2022-01-16 16:14:19 -0500

    2022 fixes and grading implementation

--- server/mail/competitors.js
+++ server/mail/competitors.js
@@ -10,41 +10,54 @@ async function competitorMail(emailAddress, teams, individuals) {
     Dear coaches,
     <br /> <br />
 
-    The EMCC will be occurring on Saturday, February 20th. 
+    The EMCC will be occurring on Saturday, January 29th.
     <br /> <br />
 
     <b>All coaches should read the <a href="https://docs.google.com/document/d/1CpK8lj6GnBWe_u4hrSwX-f51G6McZnWeNR9WwRvdaFA/edit?usp=sharing">competitors protocol</a> and forward it to their teams/individuals. </b> If you will be proctoring the competition, then please carefully read over the <a href="https://docs.google.com/document/d/1PKe0_gteGe8ec5NLU7ruXYjWAmQ-QZjYmEsEzHeFvys/edit?usp=sharing">proctors protocol</a>.
     <br /> <br />
 
-    Furthermore, it is recommended for coaches and participants (and required for all proctors) to join the EMCC discord <a href="http://discord.com/invite/VeEAWdBtRR">here</a>, in order for us to communicate updates, announcements, and information quickly. Members of the Discord server can select a Coach, Proctor, and/or Participant role in the #roles channel. 
+    Furthermore, it is recommended for coaches and participants (and required for all proctors) to join the EMCC discord <a href="https://tinyurl.com/emcc22">here</a>, in order for us to communicate updates, announcements, and information quickly. Members of the Discord server can select a Coach, Proctor, and/or Participant role in the #roles channel.
     <br /> <br />
 
     Here is your list of registered competitors:
     <br />
     <br />
-    <b>Teams:</b> <br /> ${teams
-      .map(
-        (team) =>
-          `${team.name}: ${team.student1} ${team.student2} ${team.student3} ${team.student4}`
-      )
-      .join("<br />")}
+    <b>Teams:</b> <br /> ${
+      teams.length
+        ? teams
+            .map(
+              (team) =>
+                `${team.name}: ${[
+                  team.student1,
+                  team.student2,
+                  team.student3,
+                  team.student4
+                ]
+                  .filter((s) => s !== undefined && s.trim().length)
+                  .join(", ")}`
+            )
+            .join("<br />")
+        : "None"
+    }
     <br />
     <br />
-    <b>Individuals:</b> <br /> ${individuals
-      .map((indiv) => indiv.student)
-      .join("<br />")}
-    <br /> 
+    <b>Individuals:</b> <br /> ${
+      individuals.length
+        ? individuals.map((indiv) => indiv.student).join("<br />")
+        : "None"
+    }
+    <br />
     <br />
     Please verify that this list is correct. <b> In particular, no student should be registered in multiple teams, or both in a team and as an individual. </b>
-    If you need to make any updates, contact <a href="mailto:exetermathclub@gmail.com">exetermathclub@gmail.com</a> (do not reply to this email). 
+    If you need to make any updates, contact <a href="mailto:exetermathclub@gmail.com">exetermathclub@gmail.com</a> (do not reply to this email).
     <br /> <br />
-    
+
     Sincerely,
     <br /> <br />
 
     The EMCC team
 
-    <br /> 
+    <br />
     <br />
     `
   });
--- server/scripts/competitorEmails.js
+++ server/scripts/competitorEmails.js
@@ -10,7 +10,13 @@ const readline = require("readline").createInterface({
 readline.question(
   "This will send an email to all the coaches. Type 'YES` to confirm: ",
   (password) => {
-    if (password === "YES") {
+    if (
+      password === "YES" ||
+      password === "yes" ||
+      password === "Yes" ||
+      password === "Y" ||
+      password === "y"
+    ) {
       run();
       readline.close();
     } else {
@@ -20,6 +26,7 @@ readline.question(
 );
 
 function run() {
+  let work = true;
   base("Coaches")
     .select({
       maxRecords: 1000
@@ -28,43 +35,43 @@ function run() {
       async function page(records, fetchNextPage) {
         // console.log("hello");
         // // This function (`page`) will get called for each page of records.
-
         records.forEach(async function (record) {
           if (
             record.fields["Competitors"] &&
             record.fields["Competitors"].length
           ) {
             // console.log("Retrieved", record.get("Email"));
-            // if (record.fields["Email"] === "nchowdhury@exeter.edu") {
-            const teams = [];
-            const individuals = [];
-            const competitors = await base("Competitors")
-              .select({
-                filterByFormula: `{Coach Email} = '${record.fields["Email"]}'`
-              })
-              .firstPage();
+            if (record.fields["Email"] === "danplo@bergen.org") {
+              const teams = [];
+              const individuals = [];
+              const competitors = await base("Competitors")
+                .select({
+                  filterByFormula: `{Coach Email} = '${record.fields["Email"]}'`
+                })
+                .firstPage();
 
-            for (let competitor of competitors) {
-              if (competitor.fields.Individual) {
-                individuals.push({
-                  id: competitor.id,
-                  student: competitor.fields["Student 1"]
-                });
-              } else {
-                teams.push({
-                  id: competitor.id,
-                  name: competitor.fields.Name,
-                  student1: competitor.fields["Student 1"],
-                  student2: competitor.fields["Student 2"],
-                  student3: competitor.fields["Student 3"],
-                  student4: competitor.fields["Student 4"]
-                });
+              for (let competitor of competitors) {
+                if (competitor.fields.Individual) {
+                  individuals.push({
+                    id: competitor.id,
+                    student: competitor.fields["Student 1"]
+                  });
+                } else {
+                  teams.push({
+                    id: competitor.id,
+                    name: competitor.fields.Name,
+                    student1: competitor.fields["Student 1"],
+                    student2: competitor.fields["Student 2"],
+                    student3: competitor.fields["Student 3"],
+                    student4: competitor.fields["Student 4"]
+                  });
+                }
               }
+              console.log(record.get("Email"));
+              //await competitorsMail(record.fields.Email, teams, individuals);
+              await competitorsMail("abu@exeter.edu", teams, individuals);
             }
-            console.log(record.get("Email"));
-            await competitorsMail(record.fields.Email, teams, individuals);
           }
-          // }
         });
 
         // // To fetch the next page of records, call `fetchNextPage`.
